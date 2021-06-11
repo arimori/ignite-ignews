cbc159c68f8f0710b51f003ae164137cfc92ae0e
NaoshiAC <naoshiarimori@gmail.com>
2021-06-11 11:21:57 -0300

    fix(unit test): changed jest method to check texts values on screen

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+// yarn add jest jest-dom @testing-library/jest-dom @testing-library/dom @testing-library/react babel-jest -D
+// for css: yarn add identity-obj-proxy -D
+module.exports = {
+  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: [
+    "<rootDir>/src/tests/setupTests.ts"
+  ],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
+  },
+  moduleNameMapper: {
+    "\\.(scss|css|sass)$": "identity-obj-proxy"
+  }
+}
