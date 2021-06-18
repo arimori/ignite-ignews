2db262b003cab3e4c28700d785b25b8910bbc052
NaoshiAC <naoshiarimori@gmail.com>
2021-06-17 22:43:42 -0300

    feat(Page Posts unit tests): add loads initial data unit test.

--- src/tests/pages/Posts.spec.tsx
+++ src/tests/pages/Posts.spec.tsx
@@ -1,10 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import { mocked } from 'ts-jest/utils';
 import Posts, { getStaticProps } from '../../pages/posts';
+import { getPrismicClient } from "../../services/prismic";
 
 const posts = [
   { slug: 'my-new-post', title: 'My New Post', excerpt: 'Post excerpt', updatedAt: '10 de Abril' }
-]
+];
+
+jest.mock('../../services/prismic');
 
 describe('Posts page', () => {
   it('renders correctly', () => {
@@ -13,28 +16,43 @@ describe('Posts page', () => {
     expect(screen.getByText("My New Post")).toBeInTheDocument();
   });
 
-  // it('loads initial data', async () => {
-  //   const retrieveStripePricesMocked = mocked(stripe.prices.retrieve)
+  it('loads initial data', async () => {
+    const getPrismicClientMocked = mocked(getPrismicClient);
 
-  //   retrieveStripePricesMocked.mockResolvedValueOnce({
-  //     id: 'fake-price-id',
-  //     unit_amount: 1000,
-  //   } as any);
+    getPrismicClientMocked.mockReturnValueOnce({
+      query: jest.fn().mockResolvedValueOnce({
+        results: [
+          {
+            uid: 'my-new-post',
+            data: {
+              title: [
+                { type: 'heading', text: 'My new post' }
+              ],
+              content: [
+                { type: 'paragraph', text: 'Post excerpt' }
+              ]
+            },
+            last_publication_date: '04-01-2021'
+          }
+        ]
+      })
+    } as any);
 
-  //   const response = await getStaticProps({})
+    const response = await getStaticProps({})
 
-  //   // console.log(response);
-
-  //   expect(response).toEqual(
-  //     expect.objectContaining({
-  //       props: {
-  //         product: {
-  //           priceId: 'fake-price-id',
-  //           amount: '$10.00'
-  //         }
-  //       },
-  //       revalidate: 86400
-  //     })
-  //   )
-  // })
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          posts: [
+            {
+              slug: 'my-new-post',
+              title: 'My new post',
+              excerpt: 'Post excerpt',
+              updatedAt: '01 de abril de 2021'
+            }
+          ]
+        }
+      })
+    )
+  })
 })
