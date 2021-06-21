999e3e32c6520620d10c1ea55532c4d8918318a7
NaoshiAC <naoshiarimori@gmail.com>
2021-06-21 20:18:24 -0300

    feat(Page Post unit tests): add loads initial data.

--- src/tests/pages/Post.spec.tsx
+++ src/tests/pages/Post.spec.tsx
@@ -36,5 +36,45 @@ describe('Posts page', () => {
         })
       })
     );
-  })
-})
+  });
+
+  it('loads initial data', async () => {
+    const getSessionMocked = mocked(getSession);
+    const getPrismicClientMocked = mocked(getPrismicClient);
+
+    getPrismicClientMocked.mockReturnValueOnce({
+      getByUID: jest.fn().mockResolvedValueOnce({
+        data: {
+          title: [
+            { type: 'heading', text: 'My new post' }
+          ],
+          content: [
+            { type: 'paragraph', text: 'Post content' }
+          ]
+        },
+        last_publication_date: '04-01-2021'
+      })
+    } as any);
+
+
+    getSessionMocked.mockResolvedValueOnce({
+      activeSubscription: 'fake-active-subscription'
+    } as any);
+
+    const response = await getServerSideProps({ params: { slug: 'my-new-post' } } as any);
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          post: {
+            slug: 'my-new-post',
+            title: 'My new post',
+            content: '<p>Post content</p>',
+            updatedAt: '01 de abril de 2021',
+          }
+        }
+      })
+    );
+
+  });
+});
